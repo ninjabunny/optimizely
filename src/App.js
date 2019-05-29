c6e9f76f51a8fdc9eed8b1a9a43d4e203fd609f9
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2019-05-28 20:22:47 -0700

    added exp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {
+  OptimizelyExperiment,
+  OptimizelyVariation,
+} from '@optimizely/react-sdk'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <OptimizelyExperiment experiment='exp1'>
+        <OptimizelyVariation variation='simple'>So Simple</OptimizelyVariation>
+
+        <OptimizelyVariation variation='detailed'>
+          Very Complex
+        </OptimizelyVariation>
+
+        <OptimizelyVariation default>Default Render</OptimizelyVariation>
+      </OptimizelyExperiment>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { OptimizelyProvider } from '@optimizely/react-sdk'
+import * as optimizelySDK from '@optimizely/js-web-sdk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const optimizely = optimizelySDK.createInstance({
+  datafile: window.datafile,
+})
+ReactDOM.render(
+  <OptimizelyProvider optimizely={optimizely} userId={window.userId}>
+    <App />
+  </OptimizelyProvider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
